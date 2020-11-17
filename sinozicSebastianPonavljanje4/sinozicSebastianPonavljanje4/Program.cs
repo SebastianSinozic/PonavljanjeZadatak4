10a24f236edfb50bb0abec8ee6dacd97f4069d46
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-11-17 10:46:35 +0100

    Add files via upload

---
+++ sinozicSebastianPonavljanje4/sinozicSebastianPonavljanje4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianPonavljanje4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Price of item: ");
+            string itemPrice = Console.ReadLine();
+            Console.Write("Enter tax rate (in percentage): ");
+            string taxRate = Console.ReadLine();
+
+
+            TaxCalculator myTaxCalculator = new TaxCalculator(itemPrice, taxRate);
+            myTaxCalculator.CalculateTotalPrice();
+            myTaxCalculator.GetTotalMsg();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sinozicSebastianPonavljanje4/sinozicSebastianPonavljanje4/TaxCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianPonavljanje4
+{
+    public class TaxCalculator
+    {
+        private static decimal _itemPrice;
+        private static decimal _percentTaxRate;
+        private static decimal _totalPrice;
+
+        public TaxCalculator(string inputItemPrice, string inputTaxRate)
+        {
+            _itemPrice = Decimal.Parse(inputItemPrice);
+            _percentTaxRate = Decimal.Parse(inputTaxRate) / 100;
+        }
+
+        public void CalculateTotalPrice()
+        {
+            _totalPrice = _itemPrice + (_itemPrice * _percentTaxRate);
+        }
+
+        public void GetTotalMsg()
+        {
+            Console.WriteLine("The subtotal is {0:C} and the total price is {1:C} at the tax rate of {2:p0}", _itemPrice, _totalPrice, _percentTaxRate);
+        }
+    }
+}
